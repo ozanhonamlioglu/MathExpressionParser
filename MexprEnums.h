f6995c1b62583bff2ec4a27436ebaff8390d298d
Ozan Honamlioglu <ozanhonamlioglu@gmail.com>
2024-05-10 23:33:37 +0300

    regexp enhancements

--- MexprEnums.h
+++ MexprEnums.h
@@ -5,17 +5,115 @@
 #ifndef MATHEXPRESSIONPARSER_MEXPRENUMS_H
 #define MATHEXPRESSIONPARSER_MEXPRENUMS_H
 
-typedef enum {
-    MATH_CPP_INT,
-    MATH_CPP_DOUBLE,
-    MATH_CPP_VARIABLE, // alphanumeric word
-    MATH_CPP_IPV4, // 10.1.1.1
+#include <assert.h>
 
+/* Update the values of these enums to match with the values used by
+    External project which is using this MathExpression Library*/
+
+typedef enum mexpr_opr_enums_ {
+
+    MATH_LESS_THAN_EQ,
+    MATH_LESS_THAN,
+    MATH_GREATER_THAN,
+    MATH_EQ,
+    MATH_NOT_EQ,
+    MATH_OR,
+    MATH_AND,
+    MATH_MUL,
+    MATH_DIV,
+    MATH_SQR,
+    MATH_SQRT,
+    MATH_MAX,
+    MATH_MIN,
+    MATH_PLUS,
+    MATH_MINUS,
+    MATH_SIN,
+    MATH_COS,
+    MATH_POW,
+    /* Insert new Operators in the end*/
+
+    /* If you have good reason to change the sequence
+        of above operators, pls change the array order in
+        MexprDb.c also*/
+    MATH_BRACKET_START,
+    MATH_BRACKET_END,
+    MATH_OPR_MAX
+
+} mexpr_opr_enums_t;
+
+typedef enum mexpr_opnd_enums_ {
+
+    MATH_INTEGER_VALUE = MATH_OPR_MAX + 1,
+    MATH_DOUBLE_VALUE,
+    MATH_STRING_VALUE,
+    MATH_BOOL_VALUE,
+    MATH_OPRND_MAX
+
+} mexpr_opnd_enums_t;
+
+typedef enum mexpr_generic_enums_ {
+
+    MATH_IDENTIFIER = MATH_OPRND_MAX + 1,
+    MATH_IDENTIFIER_IDENTIFIER,
+    MATH_COMMA,
+    MATH_MAX_CODE
+
+} mexpr_generic_enums_t;
+
+#define MEXPR_LIB_MAX_CODE_USED MATH_MAX_CODE
+
+
+typedef enum mexpr_dtypes_ {
+
+    MEXPR_DTYPE_INT,
+    MEXPR_DTYPE_DOUBLE,
+    MEXPR_DTYPE_STRING,
+    MEXPR_DTYPE_BOOL,
+    MEXPR_DTYPE_MAX,
+    // Dont below below enums above MAX, it will
+    // disrupt the MexprDb
+    MEXPR_DTYPE_UNKNOWN,
+    MEXPR_DTYPE_INVALID,
     MATH_CPP_DTYPE_MAX
-} mexprecpp_dtypes_t;
 
-typedef enum {
-    PARSER_EOL = (int)MATH_CPP_DTYPE_MAX + 1
-} EXTRA_ENUMS;
+} mexpr_dtypes_t;
+
+/* Conversion of Data types ID which are parsed by lex code to internal
+    data types. Internal Dtypes are usedbecause are used as indices into an array,
+    hence, has to start from zero.*/
+static inline mexpr_dtypes_t
+mexpr_get_dtype_from_value_token_code (int token_code) {
+
+    switch (token_code) {
+
+        case MATH_INTEGER_VALUE:
+            return MEXPR_DTYPE_INT;
+        case MATH_DOUBLE_VALUE:
+            return MEXPR_DTYPE_DOUBLE;
+        case MATH_STRING_VALUE:
+            return MEXPR_DTYPE_STRING;
+        case MATH_BOOL_VALUE:
+            return MEXPR_DTYPE_BOOL;
+        default:
+            return MEXPR_DTYPE_INVALID;
+    }
+}
+
+#include <stdbool.h>
+
+typedef struct mexpr_var {
+
+    mexpr_dtypes_t dtype;
+
+    union {
+
+        int int_val;
+        double d_val;
+        unsigned char *str_val;
+        bool b_val;
+
+    } u;
+} mexpr_var_t;
+
 
 #endif //MATHEXPRESSIONPARSER_MEXPRENUMS_H
---
+++ ParserExport.h
@@ -0,0 +1,20 @@
+#ifndef __PARSER_EXPORT__
+#define __PARSER_EXPORT__
+
+#include <stdint.h>
+
+typedef struct lex_data_ {
+
+    int token_code;
+    int token_len;
+    uint8_t *token_val;
+} lex_data_t;
+
+#define MAX_STRING_SIZE 512
+
+/* Common Codes */
+#define PARSER_EOL  10000
+#define PARSER_QUIT 10001
+#define PARSER_WHITE_SPACE  10002
+
+#endif
